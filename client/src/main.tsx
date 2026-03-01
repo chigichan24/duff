8838a540b1b24010c6dc70c36cbb24c2fb9c7cc9
Kazuki Chigita <kazuki.chigichan@gmail.com>
2026-03-02 02:17:11 +0900

    chore: setup isomorphic-git and polyfills

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,9 @@
 import { createRoot } from 'react-dom/client'
+import { Buffer } from 'buffer'
 import './index.css'
 import App from './App.tsx'
+
+(window as any).Buffer = Buffer
 
 createRoot(document.getElementById('root')!).render(
   <App />
